9d4ab2b99706d0e51188761d910d733819a70f81
hyunjo01 <hyunjo10217@gmail.com>
2026-03-13 09:13:12 +0900

    refactor: BookmarkResponse dto record로 변경

--- backend/src/main/java/site/to_mato/topic/dto/response/BookmarkResponse.java
+++ backend/src/main/java/site/to_mato/topic/dto/response/BookmarkResponse.java
@@ -1,12 +1,4 @@
 package site.to_mato.topic.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class BookmarkResponse {
-
-    private boolean isBookmarked;
-
+public record BookmarkResponse(boolean isBookmarked) {
 }
