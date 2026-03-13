426aa0a1f6287ec38f683128c5609129e62611d2
nnnnnara <snls369527@naver.com>
2026-03-13 15:20:21 +0900

    feat: backend Jenkinsfule 검증 stage 추가, 헬스체크 추가

--- backend/src/main/java/site/to_mato/common/controller/HealthController.java
+++ backend/src/main/java/site/to_mato/common/controller/HealthController.java
@@ -1,5 +1,6 @@
 package site.to_mato.common.controller;
 
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("Hello, Nara");
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("status", "UP"));
     }
 }
