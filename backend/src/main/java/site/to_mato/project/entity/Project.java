4ab4156a9c15f55510a9f1c34eed5a1ac2c7574a
seoliee <dev.seoliee49@gmail.com>
2026-03-16 17:30:49 +0900

    feat: 추천 로직 관련 엔티티 추가

--- backend/src/main/java/site/to_mato/project/entity/Project.java
+++ backend/src/main/java/site/to_mato/project/entity/Project.java
@@ -1,5 +1,11 @@
 package site.to_mato.project.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import site.to_mato.user.entity.User;
+import site.to_mato.topic.entity.ChildTopic;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -13,4 +19,44 @@ public class Project {
     @Column(name = "project_id")
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "started_at")
+    private LocalDateTime startedAt;
+
+    @Column(name = "due_at")
+    private LocalDateTime dueAt;
+
+    @Column(name = "invite_code")
+    private String inviteCode;
+
+    @Column(name = "topic_state")
+    private Boolean topicState;
+
+    @Column(name = "last_processed_action_log_id")
+    private Long lastProcessedActionLogId;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "preference_embeddings")
+    private List<Float> preferenceEmbeddings;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User ownerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "confirmed_child_topic_id", nullable = true)
+    private ChildTopic confirmedChildTopicId;
 }
