16a7d202c45806fbe89f0b7b8cdd584d2dcdede3
seoliee <dev.seoliee49@gmail.com>
2026-03-16 17:30:49 +0900

    feat: 추천 로직 관련 엔티티 추가

---
+++ backend/src/main/java/site/to_mato/project/entity/ProjectDomain.java
@@ -0,0 +1,35 @@
+package site.to_mato.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import site.to_mato.catalog.entity.Domain;
+
+@Getter
+@Entity
+@Table(name = "project_domains")
+public class ProjectDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_domain_id")
+    private Long id;
+
+    // @Column(name = "weight")
+    // private Long weight;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "domain_id", nullable = false)
+    private Domain domain;
+}
---
+++ backend/src/main/java/site/to_mato/topic/entity/ChildTopic.java
@@ -0,0 +1,38 @@
+package site.to_mato.topic.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import site.to_mato.project.entity.Project;
+
+@Getter
+@Setter
+@Table(name = "child_topics")
+public class ChildTopic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "child_topic_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topicId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project projectId;
+}
