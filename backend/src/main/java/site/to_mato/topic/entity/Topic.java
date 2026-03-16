0895391800c50abf716782f909b95e4b140879d0
seoliee <dev.seoliee49@gmail.com>
2026-03-16 17:31:23 +0900

    feat: 추천 로직 관련 엔티티 추가

--- backend/src/main/java/site/to_mato/topic/entity/Topic.java
+++ backend/src/main/java/site/to_mato/topic/entity/Topic.java
@@ -13,4 +13,28 @@ public class Topic {
     @Column(name = "topic_id")
     private Long id;
 
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "expected_duration_week")
+    private int expectedDurationWeek;
+
+    @Column(name = "recommended_team_size")
+    private int recommendedTeamSize;
+
+    @Column(name = "difficulty")
+    private int difficulty;
+
+    @Column(name = "tech_stacks")
+    private String techStacks;
+
+    @Column(name = "domain_id")
+    private Long domainId;
+
+    @Column(name = "source_repo_id")
+    private Long sourceRepoId;
+
 }
