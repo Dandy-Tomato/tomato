648da5930100dd509a58e9437c8706bf6c3623c7
seoliee <dev.seoliee49@gmail.com>
2026-03-17 16:08:50 +0900

    ChildTopic 엔티티 어노테이션 추가

--- backend/src/main/java/site/to_mato/topic/entity/ChildTopic.java
+++ backend/src/main/java/site/to_mato/topic/entity/ChildTopic.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import site.to_mato.project.entity.Project;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "child_topics")
 public class ChildTopic {
 
