6971226a8ef354fe386c983bdb80e21934e3711a
seoliee <dev.seoliee49@gmail.com>
2026-03-19 18:26:14 +0900

    fix: preference embedding null일 때 발생하는 에러 조치

--- backend/src/main/java/site/to_mato/project/repository/ProjectDomainRepository.java
+++ backend/src/main/java/site/to_mato/project/repository/ProjectDomainRepository.java
@@ -3,11 +3,14 @@ package site.to_mato.project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.to_mato.project.entity.ProjectDomain;
 
 public interface ProjectDomainRepository extends JpaRepository<ProjectDomain, Long> {
 
     // project_id로 project_domain 조회
-    List<ProjectDomain> findByProjectId(Long projectId);
+    @Query("SELECT pd FROM ProjectDomain pd JOIN FETCH pd.domain WHERE pd.project.id = :projectId")
+    List<ProjectDomain> findByProjectId(@Param("projectId") Long projectId);
 
 }
