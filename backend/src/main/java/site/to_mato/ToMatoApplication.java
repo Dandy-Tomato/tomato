10226ddf1f3bbd4118d8f5d115c79999c1847af1
hyunjo01 <hyunjo10217@gmail.com>
2026-03-24 09:50:20 +0900

    feat: batch 환경 구성

--- backend/src/main/java/site/to_mato/ToMatoApplication.java
+++ backend/src/main/java/site/to_mato/ToMatoApplication.java
@@ -2,8 +2,10 @@ package site.to_mato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ToMatoApplication {
 
 	public static void main(String[] args) {
