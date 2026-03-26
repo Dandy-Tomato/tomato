725d76a045b33e03f8ecae7eb4105059058bb474
hyunjo01 <hyunjo10217@gmail.com>
2026-03-26 10:44:45 +0900

    refactor: 주제 구체화 api 수정

--- backend/src/main/java/site/to_mato/topic/controller/ChildTopicController.java
+++ backend/src/main/java/site/to_mato/topic/controller/ChildTopicController.java
@@ -1,12 +1,7 @@
 package site.to_mato.topic.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.to_mato.common.response.ApiResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import site.to_mato.topic.dto.response.ChildTopicDetailResponse;
@@ -20,12 +15,12 @@ import site.to_mato.topic.service.ChildTopicService;
 public class ChildTopicController {
     private final ChildTopicService childTopicService;
 
-    @PostMapping("/refine/{topicId}/{option}")
+    @PostMapping("/refine/{topicId}")
     public ApiResponse<RefinedTopicResponse> refineTopic(
             @AuthenticationPrincipal Long userId,
             @PathVariable Long projectId,
             @PathVariable Long topicId,
-            @PathVariable TopicOption option
+            @RequestParam(required = false) TopicOption option
     ) {
         RefinedTopicResponse response = childTopicService.refineTopic(userId, projectId, topicId, option);
         return ApiResponse.ok(response);
